8ec6979c10b61fac0debf178c1002f772baaa330
Sage Neil Smith <sage.neil.smith@gmail.com>
2025-08-31 21:24:15 -0600

    Add api files for now

---
+++ api/apis/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AbilitiesApi';
+export * from './AbilitiesRequiredItemsApi';
+export * from './AbilitiesRequiredLevelsApi';
+export * from './AncestriesApi';
+export * from './BackgroundsApi';
+export * from './CharacterSkillsApi';
+export * from './CharactersApi';
+export * from './ClassesApi';
+export * from './IntrospectionApi';
+export * from './InventoryApi';
+export * from './ItemsApi';
+export * from './SkillsApi';
---
+++ api/models/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Abilities';
+export * from './AbilitiesRequiredItems';
+export * from './AbilitiesRequiredLevels';
+export * from './Ancestries';
+export * from './Backgrounds';
+export * from './CharacterSkills';
+export * from './Characters';
+export * from './Classes';
+export * from './Inventory';
+export * from './Items';
+export * from './Skills';
